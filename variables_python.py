e0ea87528aa4d23fef0e2fc473832f42d8d48fd7
Juan Sierra <jsierrachaparro@gmail.com>
2023-07-06 21:50:47 -0500

    Desarrollo Codelab: variables python

---
+++ variables_python.py
@@ -0,0 +1,33 @@
+"""
+Codelab: Variables Python
+"""
+
+entero = 1
+decimal = 3.1416
+cadena = 'Mi nombre es Juan Sebastian Sierra Chaparro'
+booleano = True
+
+# i
+concatenacion = str(entero)+', ' + str(decimal) + ', '+ cadena +', '+ str(booleano)
+
+k = '%s, %s, %s, %s' % (entero, decimal, cadena, booleano)
+
+# ii
+
+"""
+Los numeros enteros (int) y decimales (float) en python no tienen limite de tamaño
+Lo anterior debido a que python por debajo se encarga de asignar más o menos memoria al número, y podemos representar prácticamente cualquier número
+"""
+
+# iii
+
+n = int(input ('ingresa la cantidad de primeros numeros pares a sumar: '))
+
+suma_pares = n*(n+1)
+print(suma_pares)
+
+# iv
+
+print (entero, decimal, cadena, booleano, concatenacion, k, suma_pares, sep = '\n') 
+
+
